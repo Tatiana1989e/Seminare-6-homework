dfcd0bc74095883d4b9b2b496002722b5530e8f6
Egorova Tatiana <tanyasha.89@mail.ru>
2022-08-24 17:57:47 +0300

    seminars6homework task43

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 
 // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
@@ -29,3 +29,51 @@ int Array(int[] arrayNumbers)
 InputNumbers(m);
 
 Console.WriteLine($"introduced numbers -> 0: {Array(arrayNumbers)} ");
+*/
+/*
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double[,] coefficient = new double[2, 2];
+double[] crossPoint = new double[2];
+
+void InputCoefficients()
+{
+  for (int i = 0; i < coefficient.GetLength(0); i++)
+  {
+    Console.Write($"enter coefficient {i+1} equation (y = k * x + b):");
+    for (int j = 0; j < coefficient.GetLength(1); j++)
+    {
+      if(j==0) Console.Write($"enter coefficient k: ");
+      else Console.Write($"enter coefficient b: ");
+      coefficient[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
+
+double[] Decision(double[,] coefficient)
+{
+  crossPoint[0] = (coefficient[1,1] - coefficient[0,1]) / (coefficient[0,0] - coefficient[1,0]);
+  crossPoint[1] = crossPoint[0] * coefficient[0,0] + coefficient[0,1];
+  return crossPoint;
+}
+
+void ShowEquation(double[,] coefficient)
+{
+  if (coefficient[0,0] == coefficient[1,0] && coefficient[0,1] == coefficient[1,1]) 
+  {
+    Console.Write($"сoincidence");
+  }
+  else if (coefficient[0,0] == coefficient[1,0] && coefficient[0,1] != coefficient[1,1]) 
+  {
+    Console.Write($"straight lines are parallel");
+  }
+  else 
+  {
+    Decision(coefficient);
+    Console.Write($"intersection points: ({crossPoint[0]}, {crossPoint[1]})");
+  }
+}
+
+InputCoefficients();
+ShowEquation(coefficient);
+*/
